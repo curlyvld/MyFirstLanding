660e4ccece80dfc0844f50746f1e6dafedcb20e1
Vladislav <124431226+curlyvld@users.noreply.github.com>
2023-04-06 00:36:59 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+const isMobile = {
+  Android:function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() || 
+      isMobile.BlackBerry() || 
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows());
+  }
+};
+
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+} else{ 
+  document.body.classList.add('_pc');
+}
+
+const menuLinks = document.querySelectorAll('.nav__menu-link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+  
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+const icon = document.querySelector('.content__burger-img');
+const menu = document.querySelector('.nav__menu');
+
+icon.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
